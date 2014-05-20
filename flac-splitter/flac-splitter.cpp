1af1e75ae20ca4f06571b1b477c2c2da97aca66d
i.Dark_Templar <darktemplar@dark-templar-archives.net>
2014-05-20 20:39:03 +0400

    Initial commit: skeleton of project

---
+++ flac-splitter/flac-splitter.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2016 i.Dark_Templar <darktemplar@dark-templar-archives.net>
+ *
+ * This file is part of DT Cue Tools.
+ *
+ * DT Cue Tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DT Cue Tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DT Cue Tools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+ 
